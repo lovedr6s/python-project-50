6f831947bf4797456bf713d76e136d38766a0683
lovedr6s <praense@icloud.com>
2024-11-09 19:37:17 +0300

    Added json reading

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse, json
 
 def main():
     parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
@@ -6,8 +6,9 @@ def main():
     parser.add_argument('second_file', help="The second configuration file")
     parser.add_argument('-f', '--format', default="plain")
     args = parser.parse_args()
+    first_file = json.load(open("/Users/lovedr6s/Desktop/Programming/python-project-50/gendiff/scripts/" + args.first_file))
+    second_file = json.load(open("/Users/lovedr6s/Desktop/Programming/python-project-50/gendiff/scripts/" + args.second_file))
     
-    print(f"Comparing {args.first_file} and {args.second_file}")
 
 if __name__ == '__main__':
     main()
