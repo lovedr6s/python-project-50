93b7613873e8936d1fa1a2cabcc4131b64809eb5
lovedr6s <praense@icloud.com>
2024-11-12 20:49:50 +0300

    Adding checking stuff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,15 +1,18 @@
-import argparse, json
+import argparse
+import json
+
 
 def main():
-    parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+    parser = argparse.ArgumentParser(
+        description="Compares two configuration files and shows a difference.")
     parser.add_argument('first_file', help="The first configuration file")
     parser.add_argument('second_file', help="The second configuration file")
     parser.add_argument('-f', '--format', default="plain")
     args = parser.parse_args()
-    first_file = json.load(open("/Users/lovedr6s/Desktop/Programming/python-project-50/gendiff/scripts/" + args.first_file))
-    second_file = json.load(open("/Users/lovedr6s/Desktop/Programming/python-project-50/gendiff/scripts/" + args.second_file))
+    first_file = json.load(open("gendiff/scripts/" + args.first_file))
+    second_file = json.load(open("gendiff/scripts/" + args.second_file))
     print(generate_diff(first_file, second_file))
-    
+
 
 def generate_diff(first_file, second_file):
     all_keys = sorted(set(first_file.keys()).union(second_file.keys()))
@@ -26,7 +29,6 @@ def generate_diff(first_file, second_file):
         elif key in second_file:
             result_of_difference.append(f" - {key}: {second_file[key]}")
     return result_of_difference
-    
 
 
 if __name__ == '__main__':
