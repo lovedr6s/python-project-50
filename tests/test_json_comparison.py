1620c30c95abe9073eaa6cf5a96d55bd51c12bc7
lovedr6s <praense@icloud.com>
2024-11-23 19:33:45 +0300

    added tests

---
+++ tests/test_json_comparison.py
@@ -0,0 +1,13 @@
+import json
+from pathlib import Path
+
+def test_compare_flat_json():
+    file1 = Path("tests/fixtures/file1.json")
+    file2 = Path("tests/fixtures/file2.json")
+
+    with file1.open() as f1, file2.open() as f2:
+        json1 = json.load(f1)
+        json2 = json.load(f2)
+
+    assert json1 != json2
+    
