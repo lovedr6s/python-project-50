37c65b6dd2e7b70a3dcba32e8644808e7f0e36ac
lovedr6s <praense@icloud.com>
2024-12-20 18:27:01 +0300

    Changed tests, removed while

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,41 +1,28 @@
 import pytest
 from gendiff import generate_diff
 
-file1 = 'tests/fixtures/nested_file1.yml'
-file2 = 'tests/fixtures/nested_file2.yml'
-file3 = 'tests/fixtures/flat_file1.json'
-file4 = 'tests/fixtures/flat_file2.json'
-file5 = 'tests/fixtures/file3.yml'
-file6 = 'tests/fixtures/file4.yml'
-file7 = 'tests/fixtures/file1.json'
-file8 = 'tests/fixtures/file2.json'
-
-nested_yml_stylish = 'tests/fixtures/nested_yml_result.txt'
-nested_yml_plain = 'tests/fixtures/nested_yml_result_plain.txt'
-nested_yml_json = 'tests/fixtures/nested_yml_result_json.txt'
-flat_json_stylish = 'tests/fixtures/result_flat.txt'
-flat_yaml_plain = 'tests/fixtures/flat_json_result.txt'
-
+BASE_PATH = "tests/fixtures/"
 
 @pytest.mark.parametrize(
     "filepath1, filepath2, formater, diff", 
         [
-        (file1, file2, 'stylish', nested_yml_stylish), 
-        (file1, file2, 'plain', nested_yml_plain),
-        (file7, file8, 'json', nested_yml_json),
-        (file3, file4, 'stylish', flat_json_stylish),
-        (file5, file6, 'plain', flat_yaml_plain),
-        (file3, file4, 'plain', flat_yaml_plain),
+        (f'{BASE_PATH}nested_file1.yml', f'{BASE_PATH}nested_file2.yml', 'stylish', f'{BASE_PATH}nested_yml_result.txt'), 
+        (f'{BASE_PATH}nested_file1.yml', f'{BASE_PATH}nested_file2.yml', 'plain', f'{BASE_PATH}nested_yml_result_plain.txt'),
+        (f'{BASE_PATH}file1.json', f'{BASE_PATH}file2.json', 'json', f'{BASE_PATH}nested_yml_result_json.txt'),
+        (f'{BASE_PATH}flat_file1.json', f'{BASE_PATH}flat_file2.json', 'stylish', f'{BASE_PATH}result_flat.txt'),
+        (f'{BASE_PATH}file3.yml', f'{BASE_PATH}file4.yml', 'plain', f'{BASE_PATH}flat_json_result.txt'),
+        (f'{BASE_PATH}flat_file1.json', f'{BASE_PATH}flat_file2.json', 'plain', f'{BASE_PATH}flat_json_result.txt'),
         ]
     )
 def test_generate_diff(filepath1, filepath2, formater, diff):
     result = generate_diff(filepath1, filepath2, formater)
-    for lines in open(diff):
-        list_of_lines = []
-        if lines in diff:
-            merge_lines = list_of_lines.append(lines)
-            expected_result = ' '.join(merge_lines)
-            assert result == expected_result
+    with open(diff, 'r') as file:
+        expected_result = file.read()
+    print("Result:")
+    print(result)
+    print("Expected:")
+    print(expected_result)
+    assert result.strip() == expected_result.strip()
 
 
 
