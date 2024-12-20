1e3a0098a30061fd7d2c6d23e26084d7b44fd7ee
lovedr6s <praense@icloud.com>
2024-12-20 18:31:48 +0300

    Changed lines

--- tests/test_test.py
+++ tests/test_test.py
@@ -5,15 +5,45 @@ BASE_PATH = "tests/fixtures/"
 
 @pytest.mark.parametrize(
     "filepath1, filepath2, formater, diff", 
-        [
-        (f'{BASE_PATH}nested_file1.yml', f'{BASE_PATH}nested_file2.yml', 'stylish', f'{BASE_PATH}nested_yml_result.txt'), 
-        (f'{BASE_PATH}nested_file1.yml', f'{BASE_PATH}nested_file2.yml', 'plain', f'{BASE_PATH}nested_yml_result_plain.txt'),
-        (f'{BASE_PATH}file1.json', f'{BASE_PATH}file2.json', 'json', f'{BASE_PATH}nested_yml_result_json.txt'),
-        (f'{BASE_PATH}flat_file1.json', f'{BASE_PATH}flat_file2.json', 'stylish', f'{BASE_PATH}result_flat.txt'),
-        (f'{BASE_PATH}file3.yml', f'{BASE_PATH}file4.yml', 'plain', f'{BASE_PATH}flat_json_result.txt'),
-        (f'{BASE_PATH}flat_file1.json', f'{BASE_PATH}flat_file2.json', 'plain', f'{BASE_PATH}flat_json_result.txt'),
-        ]
-    )
+    [
+        (
+            f'{BASE_PATH}nested_file1.yml',
+            f'{BASE_PATH}nested_file2.yml',
+            'stylish',
+            f'{BASE_PATH}nested_yml_result.txt'
+        ),
+        (
+            f'{BASE_PATH}nested_file1.yml',
+            f'{BASE_PATH}nested_file2.yml',
+            'plain',
+            f'{BASE_PATH}nested_yml_result_plain.txt'
+        ),
+        (
+            f'{BASE_PATH}file1.json',
+            f'{BASE_PATH}file2.json',
+            'json',
+            f'{BASE_PATH}nested_yml_result_json.txt'
+        ),
+        (
+            f'{BASE_PATH}flat_file1.json',
+            f'{BASE_PATH}flat_file2.json',
+            'stylish',
+            f'{BASE_PATH}result_flat.txt'
+        ),
+        (
+            f'{BASE_PATH}file3.yml',
+            f'{BASE_PATH}file4.yml',
+            'plain',
+            f'{BASE_PATH}flat_json_result.txt'
+        ),
+        (
+            f'{BASE_PATH}flat_file1.json',
+            f'{BASE_PATH}flat_file2.json',
+            'plain',
+            f'{BASE_PATH}flat_json_result.txt'
+        ),
+    ]
+)
 def test_generate_diff(filepath1, filepath2, formater, diff):
     result = generate_diff(filepath1, filepath2, formater)
     with open(diff, 'r') as file:
